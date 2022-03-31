ea28ec47bc65e5124f0586a3cabb58bd6544240f
davidepetrillo <davide96petrillo@gmail.com>
2022-03-31 13:43:15 +0200

    Add project files.

---
+++ HelloCsharp/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Gennaio");
+Console.WriteLine("Febbraio");
+Console.WriteLine("Marzo");
+Console.WriteLine("Aprile");
+Console.WriteLine("Maggio");
+Console.WriteLine("Giugno");
+Console.WriteLine("Luglio");
+Console.WriteLine("Agosto");
+Console.WriteLine("Settembre");
+Console.WriteLine("Ottobre");
+Console.WriteLine("Novembre");
+Console.WriteLine("Dicembre");
+Console.WriteLine("Inserisci il tuo mese preferito");
+var sceltaUtente = Console.ReadLine();
+Console.Write("Il tuo mese preferito è: ");
+Console.WriteLine(sceltaUtente);
+
+
